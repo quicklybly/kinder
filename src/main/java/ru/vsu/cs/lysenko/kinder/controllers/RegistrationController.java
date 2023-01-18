b195f513929e50f80f309cc33075dbf550aa9a30
quicklybly <quickly.tawer@gmail.com>
2023-01-18 17:55:20 +0300

    fix: fixed RegistrationController using controller advice exception handling

--- src/main/java/ru/vsu/cs/lysenko/kinder/controllers/RegistrationController.java
+++ src/main/java/ru/vsu/cs/lysenko/kinder/controllers/RegistrationController.java
@@ -1,13 +1,11 @@
 package ru.vsu.cs.lysenko.kinder.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vsu.cs.lysenko.kinder.dto.SignUpDTO;
-import ru.vsu.cs.lysenko.kinder.exceptions.AppException;
 import ru.vsu.cs.lysenko.kinder.services.UserService;
 
 @RestController
@@ -15,13 +13,8 @@ import ru.vsu.cs.lysenko.kinder.services.UserService;
 public class RegistrationController {
     private final UserService userService;
     @PostMapping("/sign-up")
-    public ResponseEntity<String> registration(@RequestBody SignUpDTO signUpDTO) {
-        JSONObject responseBody = new JSONObject();
-        try {
+    public ResponseEntity<Void> registration(@RequestBody SignUpDTO signUpDTO) {
             userService.signUp(signUpDTO);
-            return ResponseEntity.ok(responseBody.toString());
-        } catch (AppException e) {
-            return ResponseEntity.status(e.getStatus()).body(responseBody.put("cause", e.getMessage()).toString());
-        }
+        return ResponseEntity.noContent().build();
     }
 }
