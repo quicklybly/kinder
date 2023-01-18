ad587964db77df4f5982c3054e80b1af2f7ec20f
quicklybly <quickly.tawer@gmail.com>
2023-01-18 18:13:23 +0300

    feat: frontend: added friend list dummy implementation

---
+++ frontend/src/components/FriendList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+  <div v-for="item in items" :key="item.username">
+    {{item}}
+  </div>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+import urlConstants from "@/urlConstants";
+
+export default {
+  name: "FriendList",
+  data() {
+    return {
+      items: [],
+    }
+  },
+  mounted() {
+    this.getFriends()
+  },
+  methods: {
+    getFriends() {
+      axios.get(urlConstants.friendsURL, {
+        "Access-Control-Allow-Origin": "http://localhost:8000/",
+        withCredentials: true,
+        'Access-Control-Allow-Credentials': true
+      }).then(resp => {
+        this.items = resp.data
+      }).catch(ex => {
+        console.log(ex)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
