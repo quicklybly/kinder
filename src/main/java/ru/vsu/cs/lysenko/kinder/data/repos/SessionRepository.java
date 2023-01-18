2aaaaf248304702cce19ea519387b9634cffa00d
quicklybly <quickly.tawer@gmail.com>
2023-01-18 17:28:29 +0300

    refactor: refactored repositories

--- src/main/java/ru/vsu/cs/lysenko/kinder/data/repos/SessionRepository.java
+++ src/main/java/ru/vsu/cs/lysenko/kinder/data/repos/SessionRepository.java
@@ -3,8 +3,11 @@ package ru.vsu.cs.lysenko.kinder.data.repos;
 import org.springframework.data.repository.CrudRepository;
 import ru.vsu.cs.lysenko.kinder.data.entities.Session;
 
+import java.util.Optional;
+
 public interface SessionRepository extends CrudRepository<Session, Long> {
     void deleteByHash(String hash);
 
     Boolean existsByHash(String hash);
+    Optional<Session> findByUserId(Long userId);
 }
--- src/main/java/ru/vsu/cs/lysenko/kinder/data/repos/UserRepository.java
+++ src/main/java/ru/vsu/cs/lysenko/kinder/data/repos/UserRepository.java
@@ -1,18 +1,10 @@
 package ru.vsu.cs.lysenko.kinder.data.repos;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.vsu.cs.lysenko.kinder.data.entities.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT true FROM users WHERE username = :username")
-    Optional<Boolean> checkIfUserInRepositoryByUsername(String username);
-
-    @Query("SELECT \"password_hash\" FROM users WHERE username = :username")
-    String getPasswordByUsername(String username);
-
-    @Query("SELECT \"user_id\" FROM users WHERE username = :username")
-    Long getIdByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
