c5303b5d648958e7b4e0ea467dfb0ff09b628dcb
quicklybly <quickly.tawer@gmail.com>
2023-01-22 22:58:22 +0300

    feat: created media controller and it's service

---
+++ src/main/java/ru/vsu/cs/lysenko/kinder/controllers/MediaController.java
@@ -0,0 +1,28 @@
+package ru.vsu.cs.lysenko.kinder.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.vsu.cs.lysenko.kinder.dto.ImageDTO;
+import ru.vsu.cs.lysenko.kinder.dto.UserDTO;
+import ru.vsu.cs.lysenko.kinder.services.MediaService;
+
+@RestController
+@RequiredArgsConstructor
+public class MediaController {
+    private final MediaService mediaService;
+
+    @GetMapping(value = "/images/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getUserImages(@PathVariable Long imageId) {
+        return ResponseEntity.ok().body(mediaService.getImage(imageId));
+    }
+
+    @PostMapping("/images")
+    public ResponseEntity<ImageDTO> postImage(@AuthenticationPrincipal UserDTO user,
+                                              @RequestParam(value = "image") MultipartFile file) {
+        return ResponseEntity.ok().body(mediaService.postImage(user, file));
+    }
+}
---
+++ src/main/java/ru/vsu/cs/lysenko/kinder/services/MediaService.java
@@ -0,0 +1,61 @@
+package ru.vsu.cs.lysenko.kinder.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.vsu.cs.lysenko.kinder.data.entities.Image;
+import ru.vsu.cs.lysenko.kinder.data.repos.ImagesRepository;
+import ru.vsu.cs.lysenko.kinder.dto.ImageDTO;
+import ru.vsu.cs.lysenko.kinder.dto.UserDTO;
+import ru.vsu.cs.lysenko.kinder.exceptions.AppException;
+import ru.vsu.cs.lysenko.kinder.mappers.ImageMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+public class MediaService {
+
+    private static final String IMAGES_PATH = "/home/quicklybly/IdeaProjects/kinder/media/images";
+    private final ImagesRepository imagesRepo;
+
+    private final ImageMapper mapper;
+
+    public byte[] getImage(Long imageId) {
+        ImageDTO image = mapper.imageToImageDTO(imagesRepo.findById(imageId)
+                .orElseThrow(() -> new AppException("No such image", HttpStatus.NOT_FOUND)));
+        Path path = Paths.get(image.getPath());
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            throw new AppException("No such image", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ImageDTO postImage(UserDTO user, MultipartFile file) {
+        String basePath = IMAGES_PATH + "/" + user.getId().toString();
+        Path directoryPath = Paths.get(basePath);
+        try {
+            if (Files.notExists(directoryPath)) {
+                Files.createDirectory(directoryPath);
+            }
+            Image image = imagesRepo.save(Image.builder()
+                    .userId(user.getId())
+                    .path(generatePath(basePath, file.getOriginalFilename()))
+                    .build());
+            file.transferTo(Paths.get(image.getPath()));
+            return mapper.imageToImageDTO(image);
+        } catch (IOException | IllegalStateException e) {
+            throw new AppException("Something wrong with file system", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private String generatePath(String basePath, String fileName) {
+        return basePath + "/" + Instant.now().toString() + "-" + fileName;
+    }
+}
