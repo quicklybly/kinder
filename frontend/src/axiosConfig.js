0cd4d56668bcc331ebd05e5c841e3a1732d10479
quicklybly <quickly.tawer@gmail.com>
2023-01-26 16:07:32 +0300

    fixed: frontend: fixed profile page revisit using dummy router full path watcher

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 axios.defaults.headers.common["Access-Control-Allow-Credentials"] = true;
 axios.defaults.headers.common["withCredentials"] = true;
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "http://localhost:8080/*";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "http://localhost:8080";
--- frontend/src/components/BasePage.vue
+++ frontend/src/components/BasePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <PageHeader></PageHeader>
-    <router-view/>
+    <router-view :key="$route.fullPath"/>
   </div>
 </template>
 
